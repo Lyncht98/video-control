2a5e523685aa1156856e38f12ac04483d983ce96
Lyncht98 <58711539+Lyncht98@users.noreply.github.com>
2023-04-24 21:11:47 -0400

    Update data_gathering.py

--- data_gathering.py
+++ data_gathering.py
@@ -1,10 +1,10 @@
 # Let's play around with some videos
-
 import cv2 as cv
 import numpy as np
 import os
 
 vid = cv.VideoCapture(0)
+max_num_frames = 90
 
 fourcc = cv.VideoWriter_fourcc(*'XVID')
 clip = list()
@@ -15,7 +15,7 @@ while True:
 
     # Save the clip
     clip.append(frame)
-    if len(clip) > 90:
+    if len(clip) > max_num_frames:
         clip.pop(0)
 
     pressedKey = cv.waitKey(1)
@@ -28,11 +28,11 @@ while True:
         # Check for highest clip_num corresponding to that key press
         # This might be a little slow, but it keeps it organized and only needed for training
         dir = os.listdir()
-        outputs_chr = [f for f in dir if f.startswith('output_'+chr(pressedKey))]
+        outputs_chr = [f for f in dir if f.startswith('clip_'+chr(pressedKey))]
         next_clip_num = np.max([int(f.split('_')[2].split('.')[0]) for f in outputs_chr]) + 1 if len(outputs_chr) > 0 else 0
         
         # save the clip to video_clips folder (this will be gitignored)
-        out = cv.VideoWriter('video_clips/output_'+chr(pressedKey)+'_'+str(next_clip_num)+'.avi', fourcc, 20.0, (640, 480))
+        out = cv.VideoWriter('video_clips/clip_'+chr(pressedKey)+'_'+str(next_clip_num)+'.avi', fourcc, 20.0, (640, 480))
         for i in range(len(clip)):
             out.write(clip[i])
         out.release()
